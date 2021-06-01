c079f3c8337ec0c116fae9aa8ce83b3be7452d06
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-06-01 23:16:50 +0800

    全局配置动画OK|局部配置动画失效

--- pages/goods/_id.vue
+++ pages/goods/_id.vue
@@ -8,6 +8,16 @@ export default {
   name: "goods-detail",
   validate({ params, query }) {
     return typeof params.id === "number";
-  }
+  },
+  transition:'test'
 };
 </script>
+
+<style scoped>
+/* .test-enter-active, .test-leave-active {
+  transition: all ease 0.5;
+}
+.test-enter, .test-leave-to{
+  margin-left: -1000px;
+} */
+</style>
