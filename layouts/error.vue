98c13f46266fdc4098fa35bdb3f127a48849b4b6
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-06-01 22:49:32 +0800

    服务端validate参数校验&路由错误定制

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode">{{ error.message }}</h1>
+    <h1 v-else>应用发送异常</h1>
+    <button @click="$router.replace('/home')">跳转到首页</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // 接受错误信息 error: {statusCode,message}
+  props: ["error"],
+  mounted() {
+    console.log("error", this.error);
+  }
+};
+</script>
--- pages/goods/_id.vue
+++ pages/goods/_id.vue
@@ -3,3 +3,11 @@
     goods details
   </div>
 </template>
+<script>
+export default {
+  name: "goods-detail",
+  validate({ params, query }) {
+    return typeof params.id === "number";
+  }
+};
+</script>
